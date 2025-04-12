70893b13f59395ea554e19003d68335f9073d15a
yauhen_bialetski <yauhen_bialetski@epam.com>
2025-04-12 11:01:25 +0200

    HW3

---
+++ hw3.py
@@ -0,0 +1,26 @@
+import re
+from multiprocessing.pool import job_counter
+
+text = """homEwork:
+ tHis iz your homeWork, copy these Text to variable.
+
+
+ You NEED TO normalize it fROM letter CASEs point oF View. also, create one MORE senTENCE witH LAST WoRDS of each existING SENtence and add it to the END OF this Paragraph.
+
+
+ it iZ misspeLLing here. fix“iZ” with correct “is”, but ONLY when it Iz a mistAKE.
+
+
+ last iz TO calculate nuMber OF Whitespace characteRS in this Tex. caREFULL, not only Spaces, but ALL whitespaces. I got 87."""
+space_count  = 0
+for i in range(len(text)):
+    if text[i] == ' ' or text[i] == '\n':
+        space_count += 1
+print(space_count)
+
+textWithoutMistakes = text.replace(' iZ ', ' is ').replace(' iz ', ' is ').replace(' Iz ', ' is ')
+
+formatedText = textWithoutMistakes.lower().split('\n ')
+for i in range(len(formatedText)):
+    formatedText[i] = formatedText[i].capitalize()
+print('\n'.join(formatedText))
